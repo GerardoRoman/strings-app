a07c5e6b02142e2782f8cdf9d4eec079dc8ce7e4
Gerardo Roman <offthegrd3657@gmail.com>
2024-04-08 14:41:32 -0400

    private layout shell

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,9 +1,18 @@
+import Footer from "./footer";
+import Header from "./header";
+import NavBar from "./navbar";
+
 export default function PrivateLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
+    <div>
+      <Header />
+      <NavBar />
     <main>{children}</main>
-  )
+    <Footer />
+    </div>
+  );
 }

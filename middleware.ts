42bef019da1e2763433d1dfb95372165baa28447
Gerardo Roman <offthegrd3657@gmail.com>
2024-04-23 11:08:01 -0400

    styling the feed page

--- middleware.ts
+++ middleware.ts
@@ -5,14 +5,15 @@ export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
   const authenticatedAPIRoutes = [
-    pathname.startsWith("/api/users")
-  ]
+    pathname.startsWith("/api/users"), 
+    pathname.startsWith("/api/posts"),
+  ];
 
   if (authenticatedAPIRoutes.includes(true)) {
     const cookie = request.cookies.get("jwt-token")
 
     if (!cookie || !cookie?.value) {
-      return NextResponse.json({ error: "unauthenticated"}, {status: 401 })
+      return NextResponse.json({ error: "unauthenticated"}, {status: 401 });
     }
 
     try {
@@ -21,8 +22,8 @@ export async function middleware(request: NextRequest) {
     } catch (error) {
       console.error(error)
       return NextResponse.json(
-        { error: "internal server error"},
-        { status: 500 }
+        { error: "unauthenticated"},
+        { status: 401 }
       );
     }
   }
